47c81371be75d2a56621aea578a82c8905484e7d
Mayara <mayandronico@msn.com>
2022-09-14 15:45:34 -0300

    Rquisito 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,11 @@
-# from src.brazilian_jobs import read_brazilian_file
+# from src.brazilian_jobs import read_brazilian_fil
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result_py = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in result_py:
+        list_keys = job.keys()
+    assert 'title' in list_keys
+    assert 'salary' in list_keys
+    assert 'type' in list_keys
